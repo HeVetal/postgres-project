c249fd0c79b18583685d7aadaac23b3d09429486
Heavyve <heavyve@gmail.com>
2025-12-06 20:12:06 +0300

    add JavaRush postgres level02 task017

--- level02/task17/solution.sql
+++ level02/task17/solution.sql
@@ -1,3 +1,5 @@
 -- Выбор названия и цены товаров из категории "Appliances", отсортированных по цене по убыванию
 SELECT  product_name, price
-FROM Appliances
+FROM product
+WHERE category = Appliances
+ORDER BY price DESC
