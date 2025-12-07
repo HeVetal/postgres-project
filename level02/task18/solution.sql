8a1787a6d7420b14beb611fd6d108bba488a0ebb
Heavyve <heavyve@gmail.com>
2025-12-07 19:55:57 +0300

    add JavaRush postgres level02 task018

--- level02/task18/solution.sql
+++ level02/task18/solution.sql
@@ -1,2 +1,6 @@
 -- Выбор имен и возраста студентов старше 21 года с оценками A или B
 -- Результаты сортируются по возрасту в порядке возрастания
+SELECT name, age
+FROM students
+WHERE age > 21 AND (grade = 'A' OR grade = 'B')
+ORDER BY age ASC
