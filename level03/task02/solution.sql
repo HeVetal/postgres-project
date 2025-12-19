566f7b2d1efba34ce1ed2f84e08725b2f20cf13c
Vetal <heavyve@gmail.com>
2025-12-19 23:38:10 +0300

    add solution JavaRush SQL level03 task02

--- level03/task02/solution.sql
+++ level03/task02/solution.sql
@@ -1,2 +1,4 @@
 -- Запрос для преобразования числового значения total_amount в строку с добавлением текста " USD"
 -- и формирования нового столбца formatted_amount
+SELECT sale_id, product_name, CONCAT(total_amount::taxt, " USD") AS formatted_amount
+FROM sales
