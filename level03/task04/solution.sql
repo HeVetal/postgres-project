26f0791dd4d9e00dfdffabab17293d2be5940fca
Vetal <heavyve@gmail.com>
2025-12-25 23:41:49 +0300

    add solution JavaRush SQL level03 task04

--- level03/task04/solution.sql
+++ level03/task04/solution.sql
@@ -1,3 +1,6 @@
 -- Запрос для стандартизации email-адресов:
 -- функция LOWER() преобразует строку к нижнему регистру,
 -- функция UPPER() преобразует строку к верхнему регистру.
+SELECT LOWER(email) AS email_lower,
+       UPPER(email) AS email_upper
+FROM customers
