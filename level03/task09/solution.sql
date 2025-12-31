6fde9cd80a9103989b294bc81c54c4f9203c0839
Vetal <heavyve@gmail.com>
2025-12-31 21:54:23 +0300

    add solution JavaRush SQL level03 task09

--- level03/task09/solution.sql
+++ level03/task09/solution.sql
@@ -1,1 +1,4 @@
 -- Запрос для объединения имени и фамилии в один столбец и форматирования даты рождения
+SELECT (first_name || last_name) AS full_name,
+       TO_CHAR(birth_date, 'DD-MM-YYYY') AS formatted_birth_date
+FROM students
