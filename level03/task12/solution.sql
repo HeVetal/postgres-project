80327e3848f0255d2df402585dbb550e89ed54b8
Vetal <heavyve@gmail.com>
2026-01-07 21:23:56 +0300

    add solution JavaRush SQL level03 task012

--- level03/task12/solution.sql
+++ level03/task12/solution.sql
@@ -1,3 +1,5 @@
 -- Извлечение домена из email-адресов в таблице users
 -- POSITION('@' IN email) возвращает индекс символа '@' в строке email
 -- Добавляем +1, чтобы начать сразу после '@' и через SUBSTRING(... FROM start) получить оставшуюся часть строки
+SELECT user_id, name, email, SUBSTRING(email FROM POSITION('@' IN email) + 1)
+FROM users
