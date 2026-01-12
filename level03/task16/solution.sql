ea4f748c15242bf3077c3a673ee7afe95a16345d
Heavyve <heavyve@gmail.com>
2026-01-12 15:28:00 +0300

    add JavaRush postgres level03 task16

--- level03/task16/solution.sql
+++ level03/task16/solution.sql
@@ -1,1 +1,4 @@
 -- Запрос для сортировки заказов по дате (по возрастанию) и идентификатору клиента (по убыванию)
+SELECT order_id, customer_id, order_date, total
+FROM orders
+ORDER BY order_date ASC, customer_id DESC
