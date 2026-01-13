2c4ecc6de5de495e37b1f584ce4e1ae9024ab161
Vetal <heavyve@gmail.com>
2026-01-13 23:24:08 +0300

    add solution JavaRush SQL level03 task017

--- level03/task17/solution.sql
+++ level03/task17/solution.sql
@@ -1,3 +1,5 @@
 -- Объединение столбцов first_name и last_name в full_name
 -- Извлечение уникальных записей с использованием DISTINCT
 -- Сортировка по enrollment_year в порядке возрастания
+SELECT DISTINCT CONCAT(first_name, " ", last_name) AS full_name
+ORDER BY enrollment_year
