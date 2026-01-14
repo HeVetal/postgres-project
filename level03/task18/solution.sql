ac33e072155e36e76b63e84f18c53cca3c14dcb5
Vetal <heavyve@gmail.com>
2026-01-14 23:11:28 +0300

    add solution JavaRush SQL level03 task018

--- level03/task18/solution.sql
+++ level03/task18/solution.sql
@@ -2,3 +2,9 @@
 -- Используем DISTINCT для удаления дубликатов
 -- Преобразуем дату в формат DD.MM.YYYY с помощью TO_CHAR
 -- Сортируем по order_date в порядке убывания
+SELECT DISTINCT
+    customer_name,
+    TO_CHAR(order_date, 'DD.MM.YYYY') AS formatted_order_date,
+    total_amount
+FROM orders
+ORDER BY formatted_order_date DESC
