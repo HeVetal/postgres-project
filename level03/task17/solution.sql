2ef81451de9409615e495a7c688d09307bd408f2
Heavyve <heavyve@gmail.com>
2026-01-22 14:55:22 +0300

    add JavaRush postgres level03 task17

--- level03/task17/solution.sql
+++ level03/task17/solution.sql
@@ -1,3 +1,6 @@
 -- Объединение столбцов first_name и last_name в full_name
 -- Извлечение уникальных записей с использованием DISTINCT
 -- Сортировка по enrollment_year в порядке возрастания
+SELECT DISTINCT CONCAT(first_name, ' ', last_name ) AS full_name
+FROM students
+ORDER BY enrollment_year
