eb99d58d2cd31dfaf81dbfae97ff9a22aa1b1bf7
Vetal <heavyve@gmail.com>
2026-01-23 22:52:52 +0300

    add solution JavaRush SQL level04 task01

--- level04/task01/solution.sql
+++ level04/task01/solution.sql
@@ -1,1 +1,8 @@
 -- Вычисление новой цены со скидкой, фильтрация товаров со скидкой и сортировка по цене со скидкой
+SELECT product_name,
+       price,
+       discount_percent,
+       price - (price * discount_percent / 100) AS discounted_price
+FROM products
+WHERE discount_percent > 0
+ORDER BY discounted_price
