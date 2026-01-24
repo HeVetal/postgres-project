4f8859685d3846adcaf52014320efaaa885d45f7
Vetal <heavyve@gmail.com>
2026-01-24 19:46:30 +0300

    add solution JavaRush SQL level04 task02

--- level04/task02/solution.sql
+++ level04/task02/solution.sql
@@ -1,1 +1,4 @@
 -- Выбор пользователей с планами "premium" или "basic", у которых подписка еще активна
+SELECT *
+FROM subscriptions
+WHERE (plan_type = 'premium' OR plan_type = 'basic') AND end_date >= CURRENT_DATE
