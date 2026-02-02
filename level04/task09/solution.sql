3014451680208c2f45bd4d2baccdcbb549315638
Vetal <heavyve@gmail.com>
2026-02-02 22:59:19 +0300

    add solution JavaRush SQL level04 task09

--- level04/task09/solution.sql
+++ level04/task09/solution.sql
@@ -1,1 +1,4 @@
 -- Используем агрегатные функции MIN() и MAX() для нахождения минимального и максимального количества товаров
+SELECT MIN(quantity) AS min_quantity,
+       MAX(quantity) AS max_quantity,
+FROM products
