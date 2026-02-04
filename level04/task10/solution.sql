e2236769e4e7b9ff1dd08b31164ee1da3d9e0a2a
Vetal <heavyve@gmail.com>
2026-02-04 23:09:00 +0300

    add solution JavaRush SQL level04 task10

--- level04/task10/solution.sql
+++ level04/task10/solution.sql
@@ -1,1 +1,4 @@
 -- Запрос для поиска самой ранней (MIN) и самой поздней (MAX) даты рождения в таблице users
+SELECT MIN(birth_date) AS youngest_birth_date,
+       MAX(birth_date) AS oldest_birth_date
+FROM users
