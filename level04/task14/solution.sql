0f7fac75bbaca28c5a8254be0a7c924ed418a1dc
Heavyve <heavyve@gmail.com>
2026-02-11 12:19:11 +0300

    add JavaRush postgres level04 task14

--- level04/task14/solution.sql
+++ level04/task14/solution.sql
@@ -1,3 +1,11 @@
 -- Группировка данных по product_id и вычисление общей суммы продаж
 -- Использование агрегатной функции SUM() для подсчета общей суммы продаж
 -- Применение условия HAVING для фильтрации групп, где суммарные продажи превышают 500
+SELECT product_id,
+       SUM(sales_amount) AS total_sales
+FROM
+    sales
+GROUP BY
+    product_id
+HAVING
+    SUM(sales_amount) > 500;
