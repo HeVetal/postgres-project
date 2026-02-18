d771fa4686704aacba3de57f05619229b25e6a79
Heavyve <heavyve@gmail.com>
2026-02-18 13:20:48 +0300

    add JavaRush postgres level04 task16

--- level04/task16/solution.sql
+++ level04/task16/solution.sql
@@ -1,1 +1,5 @@
 -- Группировка студентов по факультетам и фильтрация факультетов с максимальным возрастом студентов выше 23
+SELECT department, MAX(age) ASmax_age
+FROM students
+GROUP BY department
+HAVING MAX(age) > 23
