bf6b6531d0d2ba00383fe57a3f077c615375a42a
Vetal <heavyve@gmail.com>
2026-02-23 22:16:51 +0300

    add solution JavaRush SQL level04 task16

--- level04/task16/solution.sql
+++ level04/task16/solution.sql
@@ -1,4 +1,7 @@
 -- Группировка студентов по факультетам и фильтрация факультетов с максимальным возрастом студентов выше 23
+SELECT department, MAX(age) AS max_age
+FROM students
+HAVING MAX(age) > 23
 SELECT department, MAX(age) ASmax_age
 FROM students
 GROUP BY department
