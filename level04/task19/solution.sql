ac4874cd412f02362e364c21ed0a03c125f067fa
Vetal <heavyve@gmail.com>
2026-02-27 23:51:59 +0300

    add solution JavaRush SQL level04 task19

--- level04/task19/solution.sql
+++ level04/task19/solution.sql
@@ -1,3 +1,6 @@
 -- Запрос для нахождения общей суммы продаж по каждому отделу
 -- Используем агрегатную функцию SUM() для вычисления общей суммы продаж
 -- Группируем данные по столбцу department с помощью GROUP BY
+SELECT department, SUM(sale_amount) AS total_sales
+FROM sales
+GROUP BY department
