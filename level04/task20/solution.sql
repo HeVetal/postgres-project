83353761715e55817ed6eeb737fb537297202167
Vetal <heavyve@gmail.com>
2026-02-28 23:44:28 +0300

    add solution JavaRush SQL level04 task20

--- level04/task20/solution.sql
+++ level04/task20/solution.sql
@@ -2,3 +2,8 @@
 -- Используем агрегатную функцию SUM() для подсчета общей суммы заказов
 -- Фильтруем регионы, где общая сумма заказов превышает 600, с помощью HAVING
 -- Сортируем результат в порядке убывания общей суммы заказов (DESC)
+SELECT region, SUM(total_amount) AS total_sales
+FROM orders
+GROUP BY region
+HAVING SUM(total_amount) > 600
+ORDER BY total_sales DESC
