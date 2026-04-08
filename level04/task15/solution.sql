209eeca5b243f40008bd04f59ea5b646c4187029
Heavyve <heavyve@gmail.com>
2026-04-08 18:48:26 +0300

    add JavaRush postgres level04 task15

--- level04/task15/solution.sql
+++ level04/task15/solution.sql
@@ -1,1 +1,8 @@
 -- Выбор факультетов с числом студентов старше 18 больше 5
+SELECT
+    departament,
+    COUNT(*) AS student_count
+FROM students
+WHERE age > 18
+GROUP BY department
+HAVING COUNT(*) > 5
