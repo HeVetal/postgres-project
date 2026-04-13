f8576ebda6713d3f825ff596e8d7476c949fab09
Heavyve <heavyve@gmail.com>
2026-04-13 19:30:31 +0300

    add JavaRush postgres level06 task03

--- level06/task03/solution.sql
+++ level06/task03/solution.sql
@@ -1,3 +1,8 @@
 -- Используем INNER JOIN для объединения таблиц students и enrollments
 -- Объединение выполняется по столбцу student_id, который является общим для обеих таблиц
 -- Выбираем только два столбца: name из таблицы students и course_id из таблицы enrollments
+SELECT
+    students.name,
+    enrollments.course_id
+FROM students
+INNER JOIN enrollments ON students.student_id = enrollments.student_id;
