1f253708dd470796a1d1e80e17d9f4c3d0265e8b
Heavyve <heavyve@gmail.com>
2026-04-19 18:53:53 +0300

    add JavaRush postgres level06 task07

--- level06/task07/solution.sql
+++ level06/task07/solution.sql
@@ -1,3 +1,8 @@
 -- Решение задачи: вывести все продажи с использованием RIGHT JOIN
 -- RIGHT JOIN гарантирует, что будут сохранены все строки из таблицы sales (правой таблицы).
-SELECT
+SELECT s.sale_id,
+       s.product_id,
+       p.name,
+       s.quantity
+FROM products p
+RIGHT JOIN sales s ON p.product_id = s.product_id;
