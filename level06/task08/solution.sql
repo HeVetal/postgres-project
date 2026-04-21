7801e48c2a1140682468a4428364cff6f512b160
Vetal <heavyve@gmail.com>
2026-04-21 23:46:23 +0300

    add solution JavaRush SQL level6 task08

--- level06/task08/solution.sql
+++ level06/task08/solution.sql
@@ -1,2 +1,8 @@
 -- Используем RIGHT JOIN для объединения таблиц departments и employees
 -- Выбираем все отделы и фильтруем только те, в которых нет сотрудников
+SELECT
+    d.department_name,
+    e.name
+FROM employees e
+RIGHT JOIN departments d ON e.department_id = d.id
+WHERE e.id IS NULL;
