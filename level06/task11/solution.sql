f5d3ceed5326c9cdd5d1c7cc9dcc10a340f54307
Heavyve <heavyve@gmail.com>
2026-05-04 17:47:21 +0300

    add JavaRush postgres level06 task11

--- level06/task11/solution.sql
+++ level06/task11/solution.sql
@@ -1,3 +1,7 @@
 -- Используем LEFT JOIN для объединения таблиц students и enrollments
 -- COALESCE заменяет значения NULL в course_name на "Нет курса"
-SELECT
+SELECT s.id,
+       s.nsme,
+       COALESCE(e.course_name, 'Нет курса') AS course_name
+FROM students s
+LEFT JOIN enrollments e ON s.id = e.student_id;
