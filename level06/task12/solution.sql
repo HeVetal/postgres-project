90fab7258303053f95882afd02e3934866e73a4d
Heavyve <heavyve@gmail.com>
2026-05-06 17:30:57 +0300

    add JavaRush postgres level06 task12

--- level06/task12/solution.sql
+++ level06/task12/solution.sql
@@ -1,3 +1,7 @@
 -- Используем LEFT JOIN для объединения таблиц students и enrollments
 -- Проверяем, где course_name из таблицы enrollments равен NULL
 -- Подсчитываем количество таких студентов с помощью COUNT
+SELECT COUNT(*) AS students_without_courses
+    FROM students s
+LEFT JOIN enrollments e ON s.id = e.student_id
+WHERE e.course_name IS NULL;
