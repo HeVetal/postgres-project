49ef67b7f9a7c2cfecdcc4c3edefdd104fa18435
Heavyve <heavyve@gmail.com>
2026-05-22 16:55:05 +0300

    add JavaRush postgres level06 task16

--- level06/task16/solution.sql
+++ level06/task16/solution.sql
@@ -1,1 +1,5 @@
 -- Запрос для получения списка сотрудников, которые имели посещения только в текущем году
+SELECT DISTINCT e.name
+FROM employees e
+INNER JOIN attendances a ON a.employee_id = e.id
+    AND YEAR(a.attendance_date) = YEAR(CURRENT_DATE);
